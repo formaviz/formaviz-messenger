fb519d230e40b1a52c339b1a6cd51a54eef81214
olivier <oliviermalik@yahoo.fr>
2019-05-13 11:08:36 +0200

    [MOR] fix promise

--- src/controller/user.js
+++ src/controller/user.js
@@ -4,7 +4,7 @@ const { getChannelIdByName } = require('./channel.js');
 
 const inviteUser = (params, legacyToken) => {
     const content = JSON.parse(params.content.toString());
-    if (content.data == null || content.data.idChannel == null || content.data.name == null || legacyToken == null || content.data.email == null) return new Answer('ADD_USER', 'ERROR', 'Failed to add the user to the channel');
+    if (content.data == null || content.data.idChannel == null || content.data.name == null || legacyToken == null || content.data.email == null) return Promise.resolve(new Answer('ADD_USER', 'ERROR', 'Failed to add the user to the channel'));
     const data = {'token': legacyToken, 'channels':  content.data.idChannel, 'email': content.data.email};
     return new Promise((resolve,reject) => {
         request.post({'url': `${process.env.SLACK_URL}/users.admin.invite`, 'qs': data},
